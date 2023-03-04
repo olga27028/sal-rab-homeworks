817925310527df906f9131e2a6a11ffc3d7ce657
Olga <pakhomova.olga27028@mail.ru>
2023-03-05 00:18:23 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {
+        client: "",
+        order: {address, sum},
+        goods: []
+    };
+
+    data.client = `${name} ${phone}`;
+
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({ title: goods[i].title, count: goods[i].count });
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
